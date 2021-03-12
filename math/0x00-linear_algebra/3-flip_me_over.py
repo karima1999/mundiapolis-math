77fbadd9107b78b0897fa9052a2d8ec61554c3ee
karimaAITHABA <78323027+karima1999@users.noreply.github.com>
2021-03-12 14:26:46 +0100

    Update 3-flip_me_over.py

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+matrix_transpose = __import__('3-flip_me_over').matrix_transpose
 def matrix_transpose(matrix):
     matrix_t = []
     m = len(matrix)
@@ -8,10 +10,4 @@ def matrix_transpose(matrix):
             row.append(matrix[y][x])
         matrix_t.append(row)
     return matrix_t
-mat1 = [[1, 2], [3, 4]]
-print(mat1)
-print(matrix_transpose(mat1))
-mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
-print(mat2)
-print(matrix_transpose(mat2))
+
