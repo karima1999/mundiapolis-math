cfe45ef6705ff7abda8cd848fe01db84ebeda104
karimaAITHABA <78323027+karima1999@users.noreply.github.com>
2021-03-12 14:35:23 +0100

    Update 8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+mat_mul = __import__('8-ridin_bareback').mat_mul
+
+
 def mat_mul(mat1, mat2):
 
     result = []
@@ -13,10 +18,3 @@ def mat_mul(mat1, mat2):
         return result
     return None
 
-
-mat1 = [[1, 2],
-        [3, 4],
-        [5, 6]]
-mat2 = [[1, 2, 3, 4],
-        [5, 6, 7, 8]]
-print(mat_mul(mat1, mat2))
