f9f3b3017502c9fd25cda5d6bf972f4664f03d25
karimaAITHABA <78323027+karima1999@users.noreply.github.com>
2021-03-12 14:24:09 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -3,5 +3,5 @@ matrix_shape = __import__('2-size_me_please').matrix_shape
 def matrix_shape(matrix):
    if type(matrix[0]) is not list:
         return [len(matrix)]
-    else:
+   else:
         return [len(matrix)] + matrix_shape(matrix[0])
