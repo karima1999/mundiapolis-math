4a3a746eb557e040942d3c4bcf59a20b57c4a118
karimaAITHABA <78323027+karima1999@users.noreply.github.com>
2021-03-19 11:34:33 +0100

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-add_matrices2D = __import__('5-across_the_planes').add_matrices2D
 def add_matrices2D(mat1, mat2):
-    result = []
-    if (len(mat1) == len(mat2)) and (len(mat1[0]) == len(mat2[0])):
-        for x in range(len(mat1)):
-            row = []
-            for y in range(len(mat1[0])):
-                row.append(mat1[x][y] + mat2[x][y])
-            result.append(row)
-            return result
-    return None
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    res = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat1[0])):
+            row.append(mat1[i][j] + mat2[i][j])
+        res.append(row)
+    return (res)
