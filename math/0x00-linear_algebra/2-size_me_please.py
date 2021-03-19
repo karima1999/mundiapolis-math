6015adefeaa0bd10bf992d09706de86378709414
karimaAITHABA <78323027+karima1999@users.noreply.github.com>
2021-03-19 11:15:20 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -3,6 +3,6 @@ def matrix_shape(matrix):
     res = []
     res.append(len(matrix))
     res.append(len(matrix[0]))
-    if len(matrix[0]) > 2:
+        if len(matrix[0]) > 2:
         res.append(len(matrix[0][0]))
-    return (res)
+        return (res)
