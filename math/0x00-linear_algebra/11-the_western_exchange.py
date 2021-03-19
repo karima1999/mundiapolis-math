012e5b500dde2eb073f26f00e399ef5ae5a983ff
karimaAITHABA <78323027+karima1999@users.noreply.github.com>
2021-03-19 09:39:40 +0100

    Update 11-the_western_exchange.py

--- math/0x00-linear_algebra/11-the_western_exchange.py
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+np_transpose = __import__('11-the_western_exchange').np_transpose
 import numpy as np
-def np_transpose(matrix):
-    return matrix.T
+    def np_transpose(matrix):
+        return matrix.T
